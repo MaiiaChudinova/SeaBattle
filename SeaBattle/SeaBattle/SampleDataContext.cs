a3795f8a92d20d68d360e885395abf5bf532387e
MaiiaChudinova <maya.chudinova@unicyb.kiev.ua>
2020-01-05 16:20:36 +0200

    Main UserContorls skeletons added.

--- SeaBattle/SeaBattle/SampleDataContext.cs
+++ SeaBattle/SeaBattle/SampleDataContext.cs
@@ -6,5 +6,10 @@
         /// The current page of the application
         /// </summary>
         public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Welcome;
+
+        public int MaxShipWidth { get; set; } = 120;
+
+        public int CellSize { get; set; } = 30;
+
     }
 }
---
+++ SeaBattle/SeaBattle/View/Controls/FieldUserControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SeaBattle
+{
+    /// <summary>
+    /// Логика взаимодействия для FieldUserControl.xaml
+    /// </summary>
+    public partial class FieldUserControl : UserControl
+    {
+        public FieldUserControl()
+        {
+            InitializeComponent();
+
+            const int fieldSize = 10;
+            const int cellSize = 30;
+
+            for (int i = 0; i < fieldSize; ++i)
+            {
+                StackPanel sp = new StackPanel();
+
+                for (int j = 0; j < fieldSize; ++j)
+                {
+                    //Rectangle r = new Rectangle();
+
+                    // 1) identify rectangle's/button's name
+                    // 2) create this specific button style (fieldcellbutton style)
+
+                    Button b = new Button();
+
+                    b.Height = cellSize;
+                    b.Width = cellSize;
+                    //b.Background = Brushes.White;
+                    //b.BorderBrush = Brushes.Black;
+                    b.Style = FindResource("FieldCellButton") as Style;
+                    sp.Children.Add(b);
+                    
+                }
+                Field.Children.Add(sp);
+            }
+        }
+    }
+}
